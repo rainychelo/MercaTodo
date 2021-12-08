cacd1018531b0c81fd0c55f64b9142a4bacc168b
rainymarcelo <chelotdp@gmail.com>
2021-12-08 15:05:17 -0500

    Feat creation model,table,request for category and provider

---
+++ app/Http/Requests/Provider/StoreRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests\Provider;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:200|unique:providers',
+            'addres' => 'required|string|max:255',
+            'phone' => 'required|string|min:9|max:9|unique:providers'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Este campo es requerido',
+            'name.string' => 'El valor no es el correcto',
+            'name.max' => 'Solo se permite 255 caracteres',
+
+            'email.required' => 'Este campo es requerido',
+            'email.string' => 'El valor no es el correcto',
+            'email.max' => 'Solo se permite 200 caracteres',
+            'email.unique' => 'ya se encuentra registrado',
+
+            'addres.required' => 'Este campo es requerido',
+            'addres.string' => 'El valor no es el correcto',
+            'addres.max' => 'Solo se permite 255 caracteres',
+
+            'phone.required' => 'Este campo es requerido',
+            'phone.string' => 'El valor no es el correcto',
+            'phone.min' => 'Solo se permite 9 caracteres',
+            'phone.max' => 'Solo se permite 9 caracteres',
+            'phone.unique' => 'ya se encuentra registrado'
+        ];
+    }
+}
