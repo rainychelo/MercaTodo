e3eb63390fe19f32fb4216d682b5cdfdc52da163
rainymarcelo <chelotdp@gmail.com>
2021-12-13 00:32:44 -0500

    Revert "feat/admin rol and deactive user"
    This reverts commit 5c307b8f2896fb2fe2a060a11be1745f4ef2e027.

--- app/Http/Controllers/web/RegisterController.php
+++ app/Http/Controllers/web/RegisterController.php
@@ -3,29 +3,26 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use App\Models\User;
-
 
 class RegisterController extends Controller
 {
     public function create(){
         return view('auth.register');
-
     }
 
     public function store(){
 
         $this->validate(\request(),[
             'name'=>'required',
-            'email'=>'required|email|unique:Users',
+            'email'=>'required|email',
             'password'=>'required|confirmed'
             ]);
 
         $user=User::create(\request(['name','email','password']));
 
         auth()->login($user);
-
-
         return redirect()->to('/');
     }
 
